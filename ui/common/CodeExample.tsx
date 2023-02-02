14d8be4f176c5f11aeafb287d9b56dedd5a0969f
Ben Allen <benalleng@gmail.com>
2023-02-02 13:30:36 -0500

    add optional copy button

--- ui/common/CodeExample.tsx
+++ ui/common/CodeExample.tsx
@@ -1,13 +1,21 @@
 import { CopyButton } from 'ui'
 
-export default function CodeExample({ code, language }) {
+export default function CodeExample({
+  code,
+  language,
+  copy,
+}: {
+  code: string
+  language: string
+  copy?: boolean
+}) {
   return (
-    <pre className="mt-2 border-2 border-dashed border-white p-2">
+    <pre className="mt-2 border-2 border-dashed border-white p-2 w-fit">
       <code
         className={`language-${language} flex items-center justify-between pl-2 pr-0`}
       >
-        {code}
-        <CopyButton compact content={code} />
+        {code}&nbsp;
+        {copy ? <CopyButton compact content={code} /> : null}
       </code>
     </pre>
   )
